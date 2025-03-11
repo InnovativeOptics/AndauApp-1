0c1e31f8e6593ca852255af8038c994f94eb0956
InnovativeOptics <john@innovativeoptics.com>
2025-03-11 07:07:06 -0400

    Add "The user is responsible for confirming their own laser specifications"

--- ui.R
+++ ui.R
@@ -48,7 +48,9 @@ shinyUI(page_fluid(
                             h5("customerservice@andaumedical.com"),
                             h5("1-844-263-2888"))))
                    ,fluidRow(column(12,align='center',
-                                    h2(strong("Search eye protection by selecting a loupe style, and a laser device"))))
+                                    h2(strong("Search eye protection by selecting a loupe style, and a laser device"))),
+                             column(12,align='center',
+                                    h5(strong("*The user is responsible for confirming their own laser specifcations"))))
                    ),
   fluidRow(
     column(
